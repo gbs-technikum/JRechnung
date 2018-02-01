1af441c406a57dc5d861d630357847edc4e53087
Mirko Groß <mirkodietmargross@gmail.com>
2018-02-01 11:38:07 +0100

    Bibliotheken Apache POI und andere hinzugefügt

---
+++ src/Rechnung/Main.java
@@ -0,0 +1,17 @@
+package Rechnung;
+
+import org.apache.poi.POIReadOnlyDocument;
+import rtf.AdvancedRTFEditorKit;
+
+import javax.swing.*;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+
+}
