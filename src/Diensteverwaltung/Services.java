ef89fb1c304385c12dcf1bda61f9c1663850546b
Nadine Richter <dine.richter@googlemail.com>
2018-02-07 08:57:24 +0100

    Klasse Services erstellt. addService hinzugefügt. brainstroming für Diensteverwaltung hinzugefügt

---
+++ src/Diensteverwaltung/Services.java
@@ -0,0 +1,23 @@
+package Diensteverwaltung;
+
+/*
+    @author Richter 2018-02-07
+    Class which contents the servicedatabase
+*/
+
+import java.util.ArrayList;
+
+public class Services {
+
+    private ArrayList<String> services;
+
+    public Services() {
+        services = new ArrayList<>();
+    }
+
+    public void addService (String service) {
+        services.add(service);
+    }
+
+    
+}
